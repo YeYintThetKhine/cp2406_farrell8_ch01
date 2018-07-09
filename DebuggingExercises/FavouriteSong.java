e928a234e53a41c90e16e2343c8b36845fbb587a
YeYintThetKhine <30687245+YeYintThetKhine@users.noreply.github.com>
2018-07-09 14:49:08 +0800

    Practicle 1

---
+++ DebuggingExercises/FavouriteSong.java
@@ -0,0 +1,11 @@
+public class FavouriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Let it Go");
+        System.out.println("See u again");
+        System.out.println("Country Road");
+        System.out.println("Beautiful in White");
+    }
+
+}
---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,8 @@
+public class MovieQuote
+{
+/* This program displays a greeting */
+public static void main(String[] args)
+        {
+        System.out.println("Do or Do Not, There's No Try");
+        }
+}
